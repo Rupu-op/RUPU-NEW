ca918eeee7d3afa64063bb9176343f3111397919
Rupu-op <101443911+Rupu-op@users.noreply.github.com>
2022-06-05 17:43:32 +0530

    Update __init__.py

--- program/__init__.py
+++ program/__init__.py
@@ -1,1 +1,17 @@
 __version__ = "0.2.0"
+
+import time
+import logging
+
+from driver.core import me_bot
+
+logging.basicConfig(
+  filename=f'streambot-logs-RUPU.txt',
+  level=logging.INFO,
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("yt_dlp").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("PyTgCalls").setLevel(logging.ERROR)
+
+LOGS = logging.getLogger(__name__)
